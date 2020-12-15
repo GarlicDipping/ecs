bc09224945e76b7905756bb6b72b9344253afdf5
GarlicDipping <garlicdipping101@gmail.com>
2020-12-16 04:04:24 +0900

    Code Cleanup, Aggressive inlining

--- src/Garlic/GarlicEcsEntityExtensions.cs
+++ src/Garlic/GarlicEcsEntityExtensions.cs
@@ -7,30 +7,35 @@ namespace Leopotam.Ecs.Garlic
     public static class GarlicEcsEntityExtensions
     {
         /// <summary>
-        /// Gets types of all attached components.
+        /// Gets all component of base type.
         /// </summary>
-        /// <param name="entity">Entity.</param>
-        /// <param name="list">List to put results in it. if null - will be created. If not enough space - will be resized.</param>
-        /// <returns>Amount of components in list.</returns>
-        public static int GetComponentsOfBaseType<T> (in this EcsEntity entity, ref T[] list) {
-            ref var entityData = ref entity.Owner.GetEntityData (entity);
+        /// <typeparam name="T">Type of component.</typeparam>
+#if ENABLE_IL2CPP
+        [Unity.IL2CPP.CompilerServices.Il2CppSetOption (Unity.IL2CPP.CompilerServices.Option.NullChecks, false)]
+        [Unity.IL2CPP.CompilerServices.Il2CppSetOption (Unity.IL2CPP.CompilerServices.Option.ArrayBoundsChecks, false)]
+#endif
+        [MethodImpl (MethodImplOptions.AggressiveInlining)]
+        public static int GetComponentsOfBaseType<T>(in this EcsEntity entity, ref T[] list)
+        {
+            ref var entityData = ref entity.Owner.GetEntityData(entity);
 #if DEBUG
-            if (entityData.Gen != entity.Gen) { throw new Exception ("Cant touch destroyed entity."); }
+            if (entityData.Gen != entity.Gen) throw new Exception("Cant touch destroyed entity.");
 #endif
             var itemsCount = entityData.ComponentsCountX2 >> 1;
-            if (list == null || list.Length < itemsCount) {
-                list = new T[itemsCount];
-            }
+            if (list == null || list.Length < itemsCount) list = new T[itemsCount];
 
-            int foundItems = 0;
-            for (int i = 0, iMax = entityData.ComponentsCountX2; i < iMax; i += 2) {
-                var component = entity.Owner.ComponentPools[entityData.Components[i]].GetItem (entityData.Components[i + 1]);
+            var foundItems = 0;
+            for (int i = 0, iMax = entityData.ComponentsCountX2; i < iMax; i += 2)
+            {
+                var component = entity.Owner.ComponentPools[entityData.Components[i]]
+                    .GetItem(entityData.Components[i + 1]);
                 if (component is T)
                 {
                     list[foundItems] = (T) component;
                     foundItems++;
                 }
             }
+
             Array.Resize(ref list, foundItems);
             return itemsCount;
         }
@@ -38,10 +43,10 @@ namespace Leopotam.Ecs.Garlic
         public static object Get(in this EcsEntity entity, Type t)
         {
             var name = nameof(EcsEntityExtensions.Get);
-            MethodInfo method = typeof(EcsEntityExtensions).GetMethod(name, 
+            var method = typeof(EcsEntityExtensions).GetMethod(name,
                 BindingFlags.Public | BindingFlags.Static);
-            MethodInfo generic = method.MakeGenericMethod(t);
-            return generic.Invoke(entity, new object[]{entity});
+            var generic = method.MakeGenericMethod(t);
+            return generic.Invoke(entity, new object[] {entity});
         }
     }
 }
